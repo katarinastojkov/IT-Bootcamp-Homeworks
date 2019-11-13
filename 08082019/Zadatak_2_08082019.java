e59e8082cf6dac5c22767f217d002756a2706039
katarinastojkov <54957642+katarinastojkov@users.noreply.github.com>
2019-11-13 16:27:09 +0100

    Add files via upload

--- 08082019/Zadatak_2_08082019.java
+++ 08082019/Zadatak_2_08082019.java
@@ -1,22 +1,25 @@
-package pp;
+package prvanedelja;
 
 import java.util.Scanner;
 
 public class Zadatak_2_08082019 {
 
 	public static void main(String[] args) {
+		/*
+		 * Napisi program koji ce racunati obim i povrsinu pravougaonika, tako sto se
+		 * stranice ucitavaju preko konzole.
+		 */
+
 		Scanner sc = new Scanner(System.in);
 
-		double a, b, O, P;
-		System.out.print("Ucitaj stranicu a: ");
-		a = sc.nextDouble();
-		System.out.print("Ucitaj stranicu b: ");
-		b = sc.nextDouble();
+		System.out.print("Unesi stranicu a: ");
+		double a = sc.nextDouble();
+		System.out.print("Unesi stranicu b: ");
+		double b = sc.nextDouble();
 
-		O = 2 * a + 2 * b; //Formula za izracunavanje obima pravougaonika
-		P = a * b;//Formula za izracunavanje povrsine pravougaonika
-		System.out.println("Obim pravougaonika je: " + O + " a povrsina pravougaonika je: " + P);
+		double o = 2 * a + 2 * b, p = a * b; //Formule za izracunavanje obima i povrsine pravougaonika
 
+		System.out.println("Obim pravougaonika je: " + o + "," + " povrsina pravougaonika je: " + p);
 	}
 
 }
